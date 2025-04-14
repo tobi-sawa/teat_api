4a9fe9dbabf58e660da50fee3aa5b2ff6ee1c607
tobi-sawa <is0680xr@ed.ritsumei.ac.jp>
2025-04-14 17:37:26 +0900

    db

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "database-1.c14yeqwm8prw.ap-southeast-2.rds.amazonaws.com"
+DATABASE_URL = "mysql+pymysql://tobisawa/yuki0108@database-1.c14yeqwm8prw.ap-southeast-2.rds.amazonaws.com:3306/recipe_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
